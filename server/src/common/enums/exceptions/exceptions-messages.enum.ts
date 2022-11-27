8cdbeec5e35e98477eeeb6be517111154cfe5091
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-11-27 15:59:45 +0200

    Unique email validation for teacher creation and updating

--- server/src/common/enums/exceptions/exceptions-messages.enum.ts
+++ server/src/common/enums/exceptions/exceptions-messages.enum.ts
@@ -1,6 +1,7 @@
 enum ExceptionsMessages {
   DISCIPLINE_NOT_FOUD = 'Discipline not found',
   TEACHER_NOT_FOUND = 'Teacher not found',
+  TEACHER_WITH_SUCH_EMAIL_ALREADY_EXISTS = 'Teacher with such email already exists',
   INVALID_ID_FORMAT = 'Invalid id format',
 }
 

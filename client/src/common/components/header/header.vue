2e23c97ebe0578d423451d878037d4bf3b043848
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-12-03 17:55:57 +0200

    Basic header initiation

---
+++ client/src/common/components/header/header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import styles from "./styles.module.scss";
+</script>
+
+<template>
+  <div :class="styles.header">
+    <div :class="styles.headerOptions"></div>
+  </div>
+</template>

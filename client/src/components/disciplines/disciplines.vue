3cc25d55c83bb79240497d6831e3a3bf0ff8c1ea
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-12-10 14:09:12 +0200

    Disicplines display

--- client/src/components/disciplines/disciplines.vue
+++ client/src/components/disciplines/disciplines.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
+import { computed } from "vue";
 import { DisciplinesCardList } from "./components/components";
 import { useStore } from "@/hooks/hooks";
+import { DisciplinesActions } from "@/store/actions.common";
 
 import styles from "./styles.module.scss";
-import { DisciplinesActions } from "@/store/actions.common";
-import { computed } from "vue";
 
 const store = useStore();
 
@@ -14,11 +14,9 @@ console.log(store.dispatch(DisciplinesActions.GET_ALL_DISICPLINES));
 </script>
 
 <template>
-  <div :class="styles.disciplinesDashboardWrapper">
+  <div :class="styles.dashboardWrapper">
     <div :class="styles.disciplinesDashboard">
-      <div :class="styles.disciplinesList">
-        <DisciplinesCardList :cards="disciplines" />
-      </div>
+      <DisciplinesCardList :cards="disciplines" />
     </div>
   </div>
 </template>

de1d9fc82ce672cb51bc100fcb64aeaffe75b960
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-12-16 16:30:02 +0200

    Newws state

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,6 +2,7 @@ import { ENV } from "@/common/enums/enums";
 import type { InjectionKey } from "vue";
 import { createLogger, createStore, Store } from "vuex";
 import { administration } from "./administration/administration";
+import { news } from "./news/news";
 import type { RootState } from "./root-state";
 
 const isInDevelopmentEnvironment =
@@ -12,6 +13,7 @@ const key: InjectionKey<Store<RootState>> = Symbol();
 const store: Store<RootState> = createStore<RootState>({
   modules: {
     administration,
+    news,
   },
   strict: isInDevelopmentEnvironment,
   plugins: isInDevelopmentEnvironment ? [createLogger()] : [],

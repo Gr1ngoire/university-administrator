cd04361c832e63c7dd2526bf77c54cc6f7a1c386
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-12-28 23:30:45 +0200

    Basic search is done

--- client/src/components/news/news.vue
+++ client/src/components/news/news.vue
@@ -1,38 +1,53 @@
 <script lang="ts" setup>
 import { computed, ref, useStore } from "@/hooks/hooks";
 import { NewsActions } from "@/store/actions";
-import { Button } from "@/common/components/components";
+import { Button, Input } from "@/common/components/components";
 import { CreateNewsForm, NewsCardsList } from "./components/components";
 import styles from "./styles.module.scss";
 import { isAdmin } from "@/common/helpers/helpers";
 
 const store = useStore();
 const news = computed(() => store.state.news.news);
+store.dispatch(NewsActions.GET_ALL_NEWS);
 
 const newsCreationFormShowState = ref<boolean>(false);
 const handleToggle: () => void = (): void => {
   newsCreationFormShowState.value = !newsCreationFormShowState.value;
 };
 
-store.dispatch(NewsActions.GET_ALL_NEWS);
+const handleNewsSearch: (event: Event) => void = (event: Event): void => {
+  const input = event.target as HTMLInputElement;
+  store.dispatch(NewsActions.GET_ALL_NEWS, input.value);
+};
 </script>
 
 <template>
   <div :class="styles.dashboardWrapper">
-    <NewsCardsList :cards="news" />
-    <div v-if="isAdmin()" :class="styles.createNewsForm">
-      <CreateNewsForm
-        v-if="newsCreationFormShowState"
-        :onToggle="handleToggle"
+    <div :class="styles.newsSearchWrapper">
+      <Input
+        name="title"
+        nameToDisplay=""
+        type="text"
+        placeholder="Find the news"
+        :onInput="handleNewsSearch"
       />
-      <div :class="styles.toggleNewsFormButtonWrapper">
-        <Button
-          v-if="!newsCreationFormShowState"
-          type="click"
-          name="Add news"
-          action="add"
-          :onClick="handleToggle"
+    </div>
+    <div :class="styles.dashboard">
+      <NewsCardsList :cards="news" />
+      <div v-if="isAdmin()" :class="styles.createNewsForm">
+        <CreateNewsForm
+          v-if="newsCreationFormShowState"
+          :onToggle="handleToggle"
         />
+        <div :class="styles.toggleNewsFormButtonWrapper">
+          <Button
+            v-if="!newsCreationFormShowState"
+            type="click"
+            name="Add news"
+            action="add"
+            :onClick="handleToggle"
+          />
+        </div>
       </div>
     </div>
   </div>
